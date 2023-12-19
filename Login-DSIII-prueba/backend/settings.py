f9e81882efca7465d8f89a3f24a8581d20e69165
KPaola-M74 <moreno.karen@correounivalle.edu.co>
2023-12-18 23:26:48 -0500

    correcion imagen Docker13

--- Login-DSIII-prueba/backend/settings.py
+++ Login-DSIII-prueba/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ww78q+^9@=+55^%+l=nx!2uhs$izm7w^*i_nqv=i4=zk)7cc$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.225.16.218', 'tu-dominio.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
